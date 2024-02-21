237322b35929034c0a33b2f8ca50f84b3758332d
AGS Lakpahana <agslakpahana@gmail.com>
2024-02-21 21:18:46 +0530

    updated

--- ec2-check-status.py
+++ ec2-check-status.py
@@ -1,7 +1,22 @@
 #write a lamda function to check the status of the ec2 instance
+
+#create a tag as key : justaname and value : dev
+#instance id is i-04553857834cb5248
 import json
 import boto3
+
+client = boto3.client('ec2')
+
+response = client.describe_instances(
+    InstanceIds=[
+        'i-04553857834cb5248',
+    ],
+)
+
+print(response)
+
+
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
-    response = ec2.describe_instances()
-    print(response)
+    return{
+        'statusCode': 200,
+    }
